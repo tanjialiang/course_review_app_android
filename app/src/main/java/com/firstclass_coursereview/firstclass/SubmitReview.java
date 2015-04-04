edc7c8a11ca040706241cd09229bc380c9ebbe34
Roy <royweilai@gmail.com>
2015-04-04 17:57:36 -0400

    lai_wei branch

---
+++ app/src/main/java/com/firstclass_coursereview/firstclass/SubmitReview.java
@@ -0,0 +1,28 @@
+package com.firstclass_coursereview.firstclass;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+/**
+ * Created by Roy on 4/4/15.
+ */
+public class SubmitReview extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.submit_review);
+
+
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.semesters_array, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
+    }
+}
