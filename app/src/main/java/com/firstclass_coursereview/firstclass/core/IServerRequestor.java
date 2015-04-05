f679d0bace39d60ce572f97cac33d5ac750d69de
tanjialiang <jacob.jialiang.tan@gmail.com>
2015-04-04 21:20:06 -0400

    missed IServerRequestor push

---
+++ app/src/main/java/com/firstclass_coursereview/firstclass/core/IServerRequestor.java
@@ -0,0 +1,12 @@
+package com.firstclass_coursereview.firstclass.core;
+
+/**
+ * Created by jialiangtan on 4/1/15.
+ */
+public interface IServerRequestor {
+    /**
+     * gets an instance of the IServerRequestor
+     * @return an instance of the IServerRequestor
+     */
+    public IServerRequestor getInstance();
+}
